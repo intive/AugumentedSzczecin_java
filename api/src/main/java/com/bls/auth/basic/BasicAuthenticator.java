a6f2c20062667284ccd7fed8caa127b1e6073222
kksdev <kksdev@gmail.com>
2015-03-14 19:25:32 +0100

    Fixed auth (C) M. Łotysz

--- api/src/main/java/com/bls/auth/basic/BasicAuthenticator.java
+++ api/src/main/java/com/bls/auth/basic/BasicAuthenticator.java
@@ -13,31 +13,42 @@ import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentials;
 
-// TODO describe purpose of existence of this class
+/**
+ *  Basic Authenticator class using plaintext credentials
+ *  Used to authenticate resources with @Auth annotation
+ */
 public class BasicAuthenticator implements Authenticator<BasicCredentials, User> {
 
     // TODO we prefer constructor injection, immutability, use types here, userDao instead of commonDao
     private final CommonDao<User<String>, String> userDao;
 
-    @Inject // TODO we prefer javax package over google specific one...
+    @Inject
     public BasicAuthenticator(@Named("user") final CommonDao userDao) {this.userDao = userDao;}
 
-    @Override
     public Optional<User> authenticate(final BasicCredentials basicCredentials) throws AuthenticationException {
 
         String emailProvided = basicCredentials.getUsername();
         String passwordProvided = basicCredentials.getPassword();
 
+        User user = getUserForCredentials(emailProvided, passwordProvided);
+        if (user != null) return Optional.of(user);
+
+        throw new AuthenticationException("Invalid credentials");
+    }
+
+    private User getUserForCredentials(String emailProvided, String passwordProvided) {
+
         Collection<User<String>> userList = userDao.findAll();
 
         for (User u : userList) {
-            // TODO separate concerns: extract logic for authentication to some priv. method?
             if (u.getEmail().equals(emailProvided) && u.getPassword().equals(passwordProvided)) {
-                return Optional.of(u);
+                return u;
             }
         }
-
-        // TODO IMHO it should throw AuthenticationException here (and return User instead of Optional<User>) -> authenticated or GTFO :)
-        return Optional.absent();
+        
+        return null;
     }
+    
+    
 }
+
