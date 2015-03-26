2532d84854eaae1fd0aa9633bfce6f68f740ec5a
Paweł <pawelfrydrych95@gmail.com>
2015-03-26 16:32:48 +0100

    mongodbStart.sh repaired. Sorry for that.

--- mongo/mongodbStart.sh
+++ mongo/mongodbStart.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-readonly mongod='C:\Program Files\MongoDB\Server\3.0\bin\mongod'
-readonly dbpath='C:\data'
+readonly mongod='./mongodb-linux-x86_64-3.0.0/bin/mongod'
+readonly dbpath='dbfiles'
 readonly port='9090'
-
 mkdir -p $dbpath > /dev/null
 $mongod --dbpath $dbpath --port $port
