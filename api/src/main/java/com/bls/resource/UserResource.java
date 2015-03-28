715425db2ce4e8e38e5cc94c5430232f73709e29
kksdev <kksdev@gmail.com>
2015-03-28 02:08:40 +0100

    [FIX] removed redundant check for user duplicate [FIX] adapted REST paths to convention

--- api/src/main/java/com/bls/resource/UserResource.java
+++ api/src/main/java/com/bls/resource/UserResource.java
@@ -1,30 +1,22 @@
 package com.bls.resource;
 
-import java.util.Collection;
-
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import com.bls.auth.basic.BasicAuthenticator;
 import com.bls.core.user.User;
 import com.bls.dao.UserDao;
 import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Timed;
-
 import io.dropwizard.auth.Auth;
 import io.dropwizard.hibernate.UnitOfWork;
 
-import static com.google.common.base.Preconditions.checkState;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Collection;
 
 @Singleton
-@Path("/user")
+@Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
@@ -36,7 +28,7 @@ public class UserResource {
         this.userDao = userDao;
     }
 
-    @Path("/list")
+    @Path("/users")
     @GET
     @UnitOfWork
     @Timed
@@ -45,26 +37,32 @@ public class UserResource {
         return userDao.findAll();
     }
 
-    @Path("/add")
+    @Path("/user/{id}")
+    @GET
+    @UnitOfWork
+    @Timed
+    @ExceptionMetered
+    public User getById(@PathParam("id") String id) {
+        return userDao.findById(id);
+    }
+
+    @Path("/user")
     @POST
     @UnitOfWork
     @Timed
     @ExceptionMetered
     public User<String> create(@Valid final User<String> userWithPlaintextPassword) {
-
         final String hashedPassword = BasicAuthenticator.generateSafeHash(userWithPlaintextPassword.getPassword());
         final User<String> userWithHashedPassword = userWithPlaintextPassword.createUserWithHashedPassword(hashedPassword);
-        checkState(userDao.findByEmail(userWithPlaintextPassword.getEmail()).isPresent(), "User wih email: %s exists",
-                userWithPlaintextPassword.getEmail());
         return userDao.create(userWithHashedPassword);
     }
 
-    @Path("/clear")
-    @POST
+    @Path("/user/{id}")
+    @DELETE
     @UnitOfWork
     @Timed
     @ExceptionMetered
-    public void removeAll(@Auth String foo) {
-        userDao.deleteAll();
+    public void removeById(@Auth String foo, @PathParam("id") String id) {
+        userDao.deleteById(id);
     }
 }
