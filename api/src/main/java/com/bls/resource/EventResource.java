a5a43cd54c8abd579672e9ed9976465425bfda3d
kksdev <kksdev@gmail.com>
2015-03-28 02:44:22 +0100

    [FIX] adapted REST paths to convention [FIX] Fixed CRUD methods

--- api/src/main/java/com/bls/resource/EventResource.java
+++ api/src/main/java/com/bls/resource/EventResource.java
@@ -8,12 +8,15 @@ import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Timed;
 import io.dropwizard.hibernate.UnitOfWork;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.inject.Singleton;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.Collection;
 
 @Singleton
-@Path("/event")
+@Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class EventResource {
@@ -22,30 +25,52 @@ public class EventResource {
     private final String description;
     private final Location location;
 
-    public EventResource(CommonDao<Event> eventDao, String description, Location location) {
+    @Inject
+    public EventResource(@Named("event") CommonDao<Event> eventDao, String description, Location location) {
         this.eventDao = eventDao;
         this.description = description;
         this.location = location;
     }
 
-
+    @Path("/events")
+    @GET
+    @UnitOfWork
+    @Timed
+    @ExceptionMetered
+    public Collection<Event> getAll() {
+        return eventDao.findAll();
+    }
+    
+    @Path("/event")
     @POST
     @UnitOfWork
     @Timed
     @ExceptionMetered
     public Event add(final Event event){ return eventDao.create(event); }
-
-    @Path("/{id}")
-    @DELETE
+    
+    @Path("/event/{id}")
+    @GET
     @UnitOfWork
     @Timed
     @ExceptionMetered
-    public void delete(final Event event){ eventDao.delete(event); }
+    public Event get(@PathParam("id") final String id) {
+        return eventDao.findById(id);
+    }
 
-    @Path("/update")
+    @Path("/event/{id}")
     @POST
     @UnitOfWork
     @Timed
     @ExceptionMetered
-    public Event update(final Event event){ return eventDao.update(event); }
+    public Event update(@PathParam("id") final String id) {
+        Event event = eventDao.findById(id);
+        return eventDao.update(event);
+    }
+
+    @Path("/event/{id}")
+    @DELETE
+    @UnitOfWork
+    @Timed
+    @ExceptionMetered
+    public void deleteById(@PathParam("id") final String id){ eventDao.deleteById(id); }
 }
