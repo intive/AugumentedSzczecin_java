f5a170b5d8001b882058fc9c325ee18154859a5f
rsawczyn <radeksaw7@gmail.com>
2015-04-10 23:13:46 +0200

    Added PlaceMongodb and PlaceMongodbDao

---
+++ mongo/src/main/java/com/bls/mongodb/dao/PlaceMongodbDao.java
@@ -0,0 +1,22 @@
+package com.bls.mongodb.dao;
+
+import com.bls.core.place.Place;
+import com.bls.dao.PlaceDao;
+import com.bls.mongodb.core.PlaceMongodb;
+import com.mongodb.DB;
+
+import javax.inject.Inject;
+
+public class PlaceMongodbDao extends CommonMongodbDao<PlaceMongodb, Place<String>, String> implements PlaceDao<Place<String>> {
+
+    @Inject
+    public PlaceMongodbDao(final DB db) {
+        super(db);
+    }
+
+    @Override
+    protected Class<PlaceMongodb> getMongodbModelType() {
+        return PlaceMongodb.class;
+    }
+
+}
