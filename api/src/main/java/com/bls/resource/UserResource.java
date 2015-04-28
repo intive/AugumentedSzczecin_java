96591d6e8a2c7e86d745a037f6a8910f92514935
kksdev <kksdev@gmail.com>
2015-04-28 17:06:57 +0200

    Moved /tokens to separate resource

--- api/src/main/java/com/bls/resource/UserResource.java
+++ api/src/main/java/com/bls/resource/UserResource.java
@@ -3,7 +3,14 @@ package com.bls.resource;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.validation.Valid;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.bls.auth.basic.BasicAuthenticator;
