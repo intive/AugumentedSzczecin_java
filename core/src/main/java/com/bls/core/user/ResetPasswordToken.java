c6cdc3cc2d2dcb6b0d34b06abf41b07438a1c339
kksdev <kksdev@gmail.com>
2015-04-28 16:27:18 +0200

    [FIX] Token POJO

--- core/src/main/java/com/bls/core/user/ResetPasswordToken.java
+++ core/src/main/java/com/bls/core/user/ResetPasswordToken.java
@@ -1,33 +1,31 @@
 package com.bls.core.user;
 
 import com.bls.core.IdentifiableEntity;
-import org.bson.types.ObjectId;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.validation.constraints.NotNull;
-import java.math.BigInteger;
-import java.security.SecureRandom;
-import java.time.LocalDateTime;
+import java.util.Calendar;
 
 /**
  * Created by Marcin Podlodowski on 4/24/15.
  */
 public class ResetPasswordToken<K> extends IdentifiableEntity<K> {
-    // TODO move to dw config file
-    private static final long DURATION = 30;
     
     @NotNull
-    private final LocalDateTime expiryDate;
+    private final long expiryDate;
     private final String token;
 
-    public ResetPasswordToken() {
-        super((K) new ObjectId());
-//        this.expiryDate = LocalDateTime.now().plusMinutes(DURATION);
-        // FIXME Jackson can't map this
-        this.expiryDate = null;
-        this.token = TokenGenerator.getNew();
+    @JsonCreator
+    public ResetPasswordToken(@JsonProperty("id") final K id,
+                              @JsonProperty("expiryDate") final long expiryDate,
+                              @JsonProperty("token") final String token) {
+        super(id);
+        this.expiryDate = expiryDate;
+        this.token = token;
     }
-
-    public LocalDateTime getExpiryDate() {
+    
+    public long getExpiryDate() {
         return expiryDate;
     }
 
@@ -35,17 +33,7 @@ public class ResetPasswordToken<K> extends IdentifiableEntity<K> {
         return token;
     }
     
-    public static ResetPasswordToken generate() {
-        return new ResetPasswordToken();
-    }
-    
     public boolean checkExpired() {
-        return this.getExpiryDate().compareTo(LocalDateTime.now()) < 0;
-    }
-}
-
-class TokenGenerator {
-    public static String getNew() {
-        return new BigInteger(130, new SecureRandom()).toString(32);
+        return this.getExpiryDate() <= Calendar.getInstance().getTimeInMillis();
     }
 }
